cc33f0904a899608f24cf079b765661e29962d84
ViniciusBelasco <vbrafaeli@gmail.com>
2024-01-10 23:28:32 -0300

    Add formmat on save to autocmds

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -3,43 +3,44 @@ vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappin
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-require( "config.globals" )
-require( "config.options" )
-require( "config.keymaps" )
+require("config.globals")
+require("config.options")
+require("config.keymaps")
+require("config.autocmds")
 
-local opts = { 
-  defaults = {
-	  lazy = true,
-  },
-  install = {
-	  colorscheme = { "tokyonight" }
-  },
-  rtp = {
-	  disabled_plugins = {
-  "gzip",
-  "matchit",
-  "matchparen",
-  "netrwPlugin",
-  "tarPlugin",
-  "tohtml",
-  "tutor",
-  "zipPlugin",
-	  }
-  },
-  change_detection = {
-	  notify = true,
-  }
+local opts = {
+	defaults = {
+		lazy = true,
+	},
+	install = {
+		colorscheme = { "tokyonight" },
+	},
+	rtp = {
+		disabled_plugins = {
+			"gzip",
+			"matchit",
+			"matchparen",
+			"netrwPlugin",
+			"tarPlugin",
+			"tohtml",
+			"tutor",
+			"zipPlugin",
+		},
+	},
+	change_detection = {
+		notify = true,
+	},
 }
 
 require("lazy").setup("plugins", opts)
