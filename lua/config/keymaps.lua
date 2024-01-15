876f834be215881847f76d6f3ac4801f63ca2bcf
ViniciusBelasco <vbrafaeli@gmail.com>
2024-01-15 13:17:57 -0300

    Add navigation and resizing keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,13 +8,13 @@ mapkey("n", "<leader>tt", "NvimTreeToggle", { noremap = true, silent = true })
 -- Window Management
 mapkey("n", "<leader>sv", "vsplit") -- Vertical Split
 mapkey("n", "<leader>sh", "split") -- Horizontal Split
-mapkey("n", "<C-Up>", "resize +2" )
-mapkey("n", "<C-Down>", "resize -2" )
-mapkey("n", "<C-Left>", "Vertical resize -2" )
-mapkey("n", "<C-Right>", "Vertical resize -2" )
+mapkey("n", "<C-Up>", "resize +2")
+mapkey("n", "<C-Down>", "resize -2")
+mapkey("n", "<C-Left>", "Vertical resize -2")
+mapkey("n", "<C-Right>", "Vertical resize -2")
 
 -- Show Full File-Path
-mapkey( "n", "<leader>pa", "echo expand('%:p')" ) -- Show Full File Path
+mapkey("n", "<leader>pa", "echo expand('%:p')") -- Show Full File Path
 
 -- Don't know what it do yet
 mapkey("n", "<leader>sm", "MaximizerToggle") -- Toggle Minimiza
@@ -27,8 +27,8 @@ mapkey("n", "<leader>q", "q") -- Toggle Minimiza
 mapkey("n", "<leader><leader>", "so") -- Toggle Minimiza
 
 -- Indenting
-vim.keymap.set("v", "<", "<gv", { silent = true, noremap = true } )
-vim.keymap.set("v", ">", ">gv", { silent = true, noremap = true } )
+vim.keymap.set("v", "<", "<gv", { silent = true, noremap = true })
+vim.keymap.set("v", ">", ">gv", { silent = true, noremap = true })
 
 -- Move oown and up a selected line.
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -41,11 +41,20 @@ vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<leader>all", ":keepjumps normal! ggVG<cr>")
 
 -- Pane Navigation
-vim.keymap.set("n", "<C-h>", "<C-w>h", {} ) -- Navigate Left
-vim.keymap.set("n", "<C-j>", "<C-w>j", {} ) -- Navigate Down 
-vim.keymap.set("n", "<C-k>", "<C-w>k", {} ) -- Navigate UP 
-vim.keymap.set("n", "<C-l>", "<C-w>l", {} ) -- Navigate Right 
+vim.keymap.set("n", "<C-h>", "<C-w>h", {}) -- Navigate Left
+vim.keymap.set("n", "<C-j>", "<C-w>j", {}) -- Navigate Down
+vim.keymap.set("n", "<C-k>", "<C-w>k", {}) -- Navigate UP
+vim.keymap.set("n", "<C-l>", "<C-w>l", {}) -- Navigate Right
 
 -- Comments
 api.nvim_set_keymap("n", "<C-_>", "gcc", { noremap = false })
 api.nvim_set_keymap("v", "<C-_>", "gcc", { noremap = false })
+
+-- Vine finder
+--Search on current word filtering by current file extension
+vim.api.nvim_set_keymap("n", "<C-f>", ":Vine<CR>", {})
+vim.api.nvim_set_keymap("n", "<leader>f", ":call VineSeek('', '.')", {})
+
+--Open current shown match
+vim.api.nvim_set_keymap("n", "<leader><CR>", ":call VineOpen()<CR>", {})
+
