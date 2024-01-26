ded22e9461b83bc683edccb07cb4be5b285bc983
ViniciusBelasco <vbrafaeli@gmail.com>
2024-01-26 12:21:36 -0300

    Remove catppuccin colorscheme

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,17 +1,9 @@
 return {
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
-	},
-	{
-		"catppuccin/nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
-		config = function()
-			vim.cmd("colorscheme catppuccin-frappe")
-		end,
-	},
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {},
+	config = function()
+		vim.cmd("colorscheme tokyonight-storm")
+	end,
 }
