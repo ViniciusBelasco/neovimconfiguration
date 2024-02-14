2e1c65e9c312427f028fe3d906f1c66819741205
ViniciusBelasco <vbrafaeli@gmail.com>
2024-02-13 23:22:41 -0300

    Add ui for lsp commands

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,30 +1,44 @@
 local keymap = vim.keymap
 
 local config = function()
-  local telescope = require('telescope')
-  telescope.setup({
-    defaults = {
-      mappings = {
-        i = {
-          ["<C-j>"] = "move_selection_next",
-          ["<C-k>"] = "move_selection_previous",
-        },
-      },
-    },
-  })
+	local telescope = require("telescope")
+	telescope.setup({
+		defaults = {
+			mappings = {
+				i = {
+					["<C-j>"] = "move_selection_next",
+					["<C-k>"] = "move_selection_previous",
+				},
+			},
+		},
+	})
 end
 
 return {
-  'nvim-telescope/telescope.nvim',
-  lazy = false,
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = config,
-  keys = {
-    keymap.set('n', '<leader>fk', ':Telescope keymaps<CR>' ),
-    keymap.set('n', '<leader>fh', ':Telescope help_tags<CR>' ),
-    keymap.set('n', '<leader>ff', ':Telescope find_files<CR>' ),
-    keymap.set('n', '<leader>fa', ':Telescope <CR>' ),
-    keymap.set('n', '<leader>fg', ':Telescope live_grep<CR>' ),
-    keymap.set('n', '<leader>fb', ':Telescope buffers<CR>' ),
-  },
+	"nvim-telescope/telescope.nvim",
+	lazy = false,
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = config,
+	keys = {
+		keymap.set("n", "<leader>fk", ":Telescope keymaps<CR>"),
+		keymap.set("n", "<leader>fh", ":Telescope help_tags<CR>"),
+		keymap.set("n", "<leader>ff", ":Telescope find_files<CR>"),
+		keymap.set("n", "<leader>fa", ":Telescope <CR>"),
+		keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>"),
+		keymap.set("n", "<leader>fb", ":Telescope buffers<CR>"),
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
