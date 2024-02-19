a6c79e08ac196d8881fd88716fc0ae730081ecd4
ViniciusBelasco <vbrafaeli@gmail.com>
2024-02-19 18:44:34 -0300

    Adding all snippets file for clipper

--- lua/snippets/clipper.lua
+++ lua/snippets/clipper.lua
@@ -11,14 +11,14 @@ local sn = ls.snippet_node
 local fmt = require("luasnip.extras.fmt").fmt
 local rep = require("luasnip.extras").rep
 
-local snippets, autosnippets = {}, {}
+local autosnippets = {}
 
 local group = vim.api.nvim_create_augroup("Lua Snippets", { clear = true })
 local file_pattern = "*.lua"
 
-local sep = s("sep", { t("/*******************************************************************************/") })
-local func = s(
-	"func",
+local snippets = {
+  s("sep", { t("/*******************************************************************************/") }),
+  s("func",
 	fmt(
 		[[
     static function {}({})
@@ -31,11 +31,8 @@ local func = s(
 			i(3, "TODO"),
 			c(4, { t(""), i(1, ".t."), i(2, ".f."), t("var") }),
 		}
-	)
-)
-
-local proc = s(
-	"proc",
+	) ),
+  s("proc",
 	fmt(
 		[[
   static procedure {}({})
@@ -46,24 +43,16 @@ local proc = s(
 			i(1, "function_name"),
 			i(2, "parameter"),
 			i(3, "TODO"),
-		}
-	)
-)
-
-local include = s(
-	"inc",
+		} ) ),
+  s("inc",
 	fmt(
 		[[
   #include {}
 ]],
 		i(1, "module")
-	)
-)
-
-local delete = s("del", { i(1, "ALIAS"), t(" ->(RL(), DbDelete(), DbCommit(), DbUnLock(), dbSkip() )") })
-
-local foreach = s(
-	"feach",
+	) ),
+  s("del", { i(1, "ALIAS"), t(" ->(RL(), DbDelete(), DbCommit(), DbUnLock(), dbSkip() )") }),
+  s("feach",
 	fmt(
 		[[
     for each {} in {}
@@ -74,11 +63,8 @@ local foreach = s(
 			i(1, "arr"),
 			i(2, "arrs"),
 			i(3, "do something"),
-		}
-	)
-)
-
-local forloop = s(
+		} ) ),
+  s(
 	"for",
 	fmt(
 		[[
@@ -90,12 +76,8 @@ local forloop = s(
 			i(1, "var"),
 			i(2, "var"),
 			i(3, "do something"),
-		}
-	)
-)
-
-local dowhile = s(
-	"do",
+		} ) ),
+  s("do",
 	fmt(
 		[[
     do while {}->{} == {} .and. {}->( !EoF() )
@@ -110,16 +92,90 @@ local dowhile = s(
 			i(4, "ALIAS"),
 			i(5, "TODO"),
 			i(6, "ALIAS"),
-		}
-	)
-)
-
-table.insert(snippets, sep)
-table.insert(snippets, func)
-table.insert(snippets, proc)
-table.insert(snippets, include)
-table.insert(snippets, delete)
-table.insert(snippets, foreach)
-table.insert(snippets, forloop)
+		}	) ),
+  s("aadd", fmt(
+    [[AAdd( {}, {} )]],
+    {
+      i(1, "array"),
+      i(2, "value"),
+    }
+  ) ),
+  -- array works
+  s("aclone", fmt([[]],{}) ),
+  s("acopy", fmt([[]],{}) ),
+  s("adel", fmt([[]],{}) ),
+  s("aeval", fmt([[]],{}) ),
+  s("afill", fmt([[]],{}) ),
+  s("ains", fmt([[]],{}) ),
+  s("arr", fmt([[]],{}) ),
+  s("ascan", fmt([[]],{}) ),
+  s("asize", fmt([[]],{}) ),
+  s("asort", fmt([[]],{}) ),
+  s("atail", fmt([[]],{}) ),
+  s("afields", fmt([[]],{}) ),
+  -- database
+  s("bof", fmt([[]],{}) ),
+  s("dbappend", fmt([[]],{}) ),
+  s("dbclearfilter", fmt([[]],{}) ),
+  s("dbcloseall", fmt([[]],{}) ),
+  s("dbclosearea", fmt([[]],{}) ),
+  s("dbcommit", fmt([[]],{}) ),
+  s("dbcommitall", fmt([[]],{}) ),
+  s("dbcreate", fmt([[]],{}) ),
+  s("dbdelete", fmt([[]],{}) ),
+  s("dbfilter", fmt([[]],{}) ),
+  s("dbgobottom", fmt([[]],{}) ),
+  s("dbgoto", fmt([[]],{}) ),
+  s("dbgotop", fmt([[]],{}) ),
+  s("dbrecall", fmt([[]],{}) ),
+  s("dbrlock", fmt([[]],{}) ),
+  s("dbrlocklist", fmt([[]],{}) ),
+  s("dbrunlock", fmt([[]],{}) ),
+  s("dbseek", fmt([[]],{}) ),
+  s("dbselectarea", fmt([[]],{}) ),
+  s("dbskip", fmt([[]],{}) ),
+  s("dbstruct", fmt([[]],{}) ),
+  s("dbunlock", fmt([[]],{}) ),
+  s("dbunlockall", fmt([[]],{}) ),
+  s("dbusearea", fmt([[]],{}) ),
+  s("dbf", fmt([[]],{}) ),
+  s("deleted", fmt([[]],{}) ),
+  s("eof", fmt([[]],{}) ),
+  s("fcount", fmt([[]],{}) ),
+  s("fieldget", fmt([[]],{}) ),
+  s("fieldname", fmt([[]],{}) ),
+  s("fieldpos", fmt([[]],{}) ),
+  s("fieldput", fmt([[]],{}) ),
+  s("flock", fmt([[]],{}) ),
+  s("found", fmt([[]],{}) ),
+  s("header", fmt([[]],{}) ),
+  s("indexkey", fmt([[]],{}) ),
+  s("indexord", fmt([[]],{}) ),
+  s("lastrec", fmt([[]],{}) ),
+  s("reccount", fmt([[]],{}) ),
+  s("recno", fmt([[]],{}) ),
+  s("recsize", fmt([[]],{}) ),
+  s("rlock", fmt([[]],{}) ),
+  s("select", fmt([[]],{}) ),
+  s("used", fmt([[]],{}) ),
+  -- date
+  s("cdow", fmt([[]],{}) ),
+  s("cmonth", fmt([[]],{}) ),
+  s("ctod", fmt([[]],{}) ),
+  s("date", fmt([[]],{}) ),
+  s("day", fmt([[]],{}) ),
+  s("days", fmt([[]],{}) ),
+  s("dow", fmt([[]],{}) ),
+  s("dtoc", fmt([[]],{}) ),
+  s("dtos", fmt([[]],{}) ),
+  s("elaptime", fmt([[]],{}) ),
+  s("month", fmt([[]],{}) ),
+  s("seconds", fmt([[]],{}) ),
+  s("secs", fmt([[]],{}) ),
+  s("time", fmt([[]],{}) ),
+  s("year", fmt([[]],{}) ),
+  -- execution
+  s("eval", fmt([[Eval({})]],{i(1, "bBlock")}) ),
+}
 
 return snippets, autosnippets
