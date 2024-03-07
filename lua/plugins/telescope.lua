c05490641d3e2348da6583d2ed9f5e4d26ce2892
ViniciusBelasco <vbrafaeli@gmail.com>
2024-03-07 07:35:01 -0300

    Format files and fix telescope table return

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -26,20 +26,23 @@ local uiconfig = function()
 end
 
 return {
-	"nvim-telescope/telescope.nvim",
-	lazy = false,
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = config,
-	keys = {
-		keymap.set("n", "<leader>fk", ":Telescope keymaps<CR>"),
-		keymap.set("n", "<leader>fh", ":Telescope help_tags<CR>"),
-		keymap.set("n", "<leader>ff", ":Telescope find_files<CR>"),
-		keymap.set("n", "<leader>fa", ":Telescope <CR>"),
-		keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>"),
-		keymap.set("n", "<leader>fb", ":Telescope buffers<CR>"),
-	},
 	{
 		"nvim-telescope/telescope-ui-select.nvim",
 		config = uiconfig,
 	},
+	{
+		"nvim-telescope/telescope.nvim",
+		lazy = false,
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = config,
+		keys = {
+			keymap.set("n", "<leader>fk", ":Telescope keymaps<CR>"),
+			keymap.set("n", "<leader>fh", ":Telescope help_tags<CR>"),
+			keymap.set("n", "<leader>ff", ":Telescope find_files<CR>"),
+			keymap.set("n", "<leader>fa", ":Telescope <CR>"),
+			keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>"),
+			keymap.set("n", "<leader>fb", ":Telescope buffers<CR>"),
+		},
+	},
 }
+
