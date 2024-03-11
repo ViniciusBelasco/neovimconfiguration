77999799cb26b5158c52b8af275978838561e286
ViniciusBelasco <vbrafaeli@gmail.com>
2024-03-11 17:35:22 -0300

    Add surround completion

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,6 +1,18 @@
+local surround = function()
+	require("mini.surround").setup()
+end
+
 return {
-	"windwp/nvim-autopairs",
-	lazy = false,
-	event = "InsertEnter",
-	opts = {}, -- this is equalent to setup({}) function
+	{
+		"windwp/nvim-autopairs",
+		lazy = false,
+		event = "InsertEnter",
+		opts = {}, -- this is equalent to setup({}) function
+	},
+	{
+		"echasnovski/mini.surround",
+    lazy = false,
+		version = false,
+		config = surround,
+	},
 }
