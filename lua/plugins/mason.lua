34fa365cd9c00bb9fca9491a0c358ae2a4c91c6d
ViniciusBelasco <vbrafaeli@gmail.com>
2024-03-13 09:09:10 -0300

    Group up mason plugins

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,16 +1,31 @@
-return {
-	"williamboman/mason.nvim",
-	cmd = "Mason",
-	event = "BufReadPre",
-	config = function()
-		require("mason").setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
-				},
+local opts = {
+	ensure_installed = { "efm", "lua_ls" },
+	automatic_installation = true,
+}
+
+local lspfunction = function()
+	require("mason").setup({
+		ui = {
+			icons = {
+				package_installed = "✓",
+				package_pending = "➜",
+				package_uninstalled = "✗",
 			},
-		})
-	end,
+		},
+	})
+end
+
+return {
+	{
+		"williamboman/mason.nvim",
+		cmd = "Mason",
+		event = "BufReadPre",
+		config = lspfunction,
+		{
+			"williamboman/mason-lspconfig.nvim",
+			opts = opts,
+			event = "BufReadPre",
+			dependencies = "williamboman/mason.nvim",
+		},
+	},
 }
