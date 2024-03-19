c6e4fad09255c23e10e3f3348ff6fb736ab8b39f
ViniciusBelasco <vbrafaeli@gmail.com>
2024-03-19 08:54:54 -0300

    Add new features for git integration

---
+++ lua/plugins/git.lua
@@ -0,0 +1,13 @@
+return {
+	{
+		"tpope/vim-fugitive",
+		lazy = false,
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		lazy = false,
+		config = function()
+			require("gitsigns").setup({})
+		end,
+	},
+}
