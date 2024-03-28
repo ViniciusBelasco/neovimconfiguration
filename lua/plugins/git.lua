e8c4c7de519d5da631930b4ee070e28451e8a1b1
ViniciusBelasco <vbrafaeli@gmail.com>
2024-03-27 21:44:14 -0300

    Add git sings and fix linter for python

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -3,11 +3,31 @@ return {
 		"tpope/vim-fugitive",
 		lazy = false,
 	},
-	{
-		"lewis6991/gitsigns.nvim",
-		lazy = false,
+  {
+    "lewis6991/gitsigns.nvim",
+    lazy = false,
 		config = function()
 			require("gitsigns").setup({})
 		end,
-	},
+    opts = {
+      signs = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "" },
+        topdelete = { text = "" },
+        changedelete = { text = "▎" },
+        untracked = { text = "▎" },
+      },
+    },
+    on_attach = function(buffer)
+      local gs = package.loaded.gitsigns
+
+      local function map(mode, lhs, rhs, desc)
+        vim.keymap.set(mode, lhs, rhs, { buffer = buffer, desc = desc })
+      end
+
+      map("n", "]h", gs.next_hunk, "Next Hunk")
+      map("n", "[h", gs.prev_hunk, "Prev Hunk")
+    end,
+  },
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,19 +1,25 @@
 return {
-  "nvimtools/none-ls.nvim",
-  lazy = false,
-  config = function()
-    local null_ls = require("null-ls")
+	{
+		"nvimtools/none-ls.nvim",
+		lazy = false,
+		config = function()
+			local null_ls = require("null-ls")
 
-    null_ls.setup({
-      sources = {
-        --Lua
-        null_ls.builtins.formatting.stylua,
-        --python
-        null_ls.builtins.diagnostics.flake8,
-        null_ls.builtins.formatting.black,
-      },
-    })
+			null_ls.setup({
+				sources = {
+					--Lua
+					null_ls.builtins.formatting.stylua,
+					--python
+					require('none-ls.diagnostics.flake8'),
+					null_ls.builtins.formatting.isort,
+					null_ls.builtins.formatting.black,
+				},
+			})
 
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		end,
+		dependencies = {
+			"nvimtools/none-ls-extras.nvim",
+		},
+	},
 }
