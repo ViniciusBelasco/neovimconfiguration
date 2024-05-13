c8437fdb09a08a46537e7d68d490ac8090f056b3
ViniciusBelasco <vbrafaeli@gmail.com>
2024-05-12 22:56:39 -0300

    Remove colors file

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -52,7 +52,6 @@ return {
 			function Set_terminal_keymaps()
 				local opts = { buffer = 0 }
 				vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
-				vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
 				vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
 				vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
 				vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
@@ -66,5 +65,14 @@ return {
 	{
 		"mbbill/undotree",
 		lazy = false,
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+		config = function()
+			vim.cmd("colorscheme tokyonight-storm")
+		end,
 	},
 }
