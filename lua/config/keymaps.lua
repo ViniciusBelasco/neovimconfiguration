30db2e574be704221da08dc54241698662cce0ec
ViniciusBelasco <vbrafaeli@gmail.com>
2024-05-17 10:22:46 -0300

    Move telescope keymaps to keymap file

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,7 @@
 local CR = "<CR>"
 local keymaps = vim.keymap.set
 local api = vim.api.nvim_set_keymap
+local telescope = require("telescope.builtin")
 
 -- Directory Navigation
 keymaps("n", "<leader>tf", ":NvimTreeFocus" .. CR, { noremap = true, silent = true })
@@ -8,14 +9,14 @@ keymaps("n", "<leader>tt", ":NvimTreeToggle" .. CR, { noremap = true, silent = t
 
 -- Window Management
 keymaps("n", "<leader>sv", ":vsplit" .. CR) -- Vertical Split
-keymaps("n", "<leader>sh", ":split" .. CR)  -- Horizontal Split
+keymaps("n", "<leader>sh", ":split" .. CR) -- Horizontal Split
 keymaps("n", "<C-Up>", ":resize +2" .. CR)
 keymaps("n", "<C-Down>", ":resize -2" .. CR)
 keymaps("n", "<C-Right>", ":vertical resize -2" .. CR)
 keymaps("n", "<C-Left>", ":vertical resize +2" .. CR)
 
 -- Undo Tree
-keymaps("n","<leader>u", vim.cmd.UndotreeToggle)
+keymaps("n", "<leader>u", vim.cmd.UndotreeToggle)
 
 -- Show Full File-Path
 keymaps("n", "<leader>pa", ":echo expand('%:p')" .. CR) -- Show Full File Path
@@ -28,7 +29,7 @@ keymaps("n", "<leader>w", ":w" .. CR) -- save
 keymaps("n", "<leader>q", ":q" .. CR) -- quit
 
 -- Source lua files
-keymaps("n", "<leader><leader>", ":so" .. CR)                                            -- source file
+keymaps("n", "<leader><leader>", ":so" .. CR) -- source file
 -- Source snippets file
 keymaps("n", "<leader><leader>s", "<cmd>source ~/.config/nvim/after/plugin/luasnip.lua") -- source snippets
 
@@ -71,30 +72,50 @@ api("n", "<leader>" .. CR, ":call VineOpen()" .. CR, {})
 -- LuaSnip configuration
 -- This expand the current item or jump to the next item within the snippet
 keymaps({ "i", "s" }, "<C-x>", function()
-  ---@diagnostic disable-next-line: undefined-global
-  if luasnip.expand_or_jumpable() then
-    ---@diagnostic disable-next-line: undefined-global
-    luasnip.expand_or_jump()
-  end
+	---@diagnostic disable-next-line: undefined-global
+	if luasnip.expand_or_jumpable() then
+		---@diagnostic disable-next-line: undefined-global
+		luasnip.expand_or_jump()
+	end
 end)
 
 -- Trouble shooting
 -- Lua
 keymaps("n", "<leader>xx", function()
-  require("trouble").toggle()
+	require("trouble").toggle()
 end)
 keymaps("n", "<leader>xw", function()
-  require("trouble").toggle("workspace_diagnostics")
+	require("trouble").toggle("workspace_diagnostics")
 end)
 keymaps("n", "<leader>xd", function()
-  require("trouble").toggle("document_diagnostics")
+	require("trouble").toggle("document_diagnostics")
 end)
 keymaps("n", "<leader>xq", function()
-  require("trouble").toggle("quickfix")
+	require("trouble").toggle("quickfix")
 end)
 keymaps("n", "<leader>xl", function()
-  require("trouble").toggle("loclist")
+	require("trouble").toggle("loclist")
 end)
 keymaps("n", "gR", function()
-  require("trouble").toggle("lsp_references")
+	require("trouble").toggle("lsp_references")
 end)
+
+-- Telescope
+keymaps("n", "<leader>fk", function()
+	telescope.keymaps()
+end, {})
+keymaps("n", "<leader>fh", function()
+	telescope.help_tags()
+end, {})
+keymaps("n", "<leader>ff", function()
+	telescope.find_files()
+end, {})
+keymaps("n", "<leader>fa", function()
+	telescope.builtin()
+end, {})
+keymaps("n", "<leader>fg", function()
+	telescope.live_grep()
+end, {})
+keymaps("n", "<leader>fb", function()
+	telescope.buffers()
+end, {})
