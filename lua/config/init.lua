b8ba8fe1ac31d5625a1d3df2d2988a841d1f07dd
ViniciusBelasco <vbrafaeli@gmail.com>
2024-07-19 09:58:31 -0300

    Change configuration to be more like lazyvim editor

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,39 +1,40 @@
 -- Example using a list of specs with the default options
-vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
-vim.g.maplocalleader = " "
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+   -- bootstrap lazy.nvim
+   -- stylua: ignore
+	vim.fn.system({"git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local opts = {
 	defaults = {
-		lazy = true,
+      -- by default, only LazyVim plugins will be lazy-loaded. Your custom plugins will lead during startup.
+      -- If you know what you're doing, you can set to this to `true` to have all your custom plugins lazy-loaded by default
+		lazy = false,
+      -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+      -- have outdated releases, which may break your Neovim install.
+      version = true, -- always use the latest git commit
+      -- version = "*", -- try installing the latest stable version for plugins that support semver
 	},
 	install = {
-		colorscheme = { "tokyonight" },
+		colorscheme = { "tokyonight", "catppuccin" },
 	},
-	rtp = {
-		disabled_plugins = {
-			"gzip",
-			"matchit",
-			"matchparen",
-			"netrwPlugin",
-			"tarPlugin",
-			"tohtml",
-			"tutor",
-			"zipPlugin",
-		},
-	},
+   checker = { enable = true }, -- Automatically check for plugin updates
+   performance = {
+      rtp = {
+         disabled_plugins = {
+            "gzip",
+            "matchit",
+            "matchparen",
+            "netrwPlugin",
+            "tarPlugin",
+            "tohtml",
+            "tutor",
+            "zipPlugin",
+         },
+      },
+   },
 	change_detection = {
 		notify = true,
 	},
