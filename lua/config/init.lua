a6dfa67e46bb53bb4d69984b97f11540e9fb43fd
ViniciusBelasco <vbrafaeli@gmail.com>
2024-07-19 15:34:02 -0300

    Fix leader mapping

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.g.mapleader = "\\"
+vim.g.maplocalleader = "\\"
 
 -- Example using a list of specs with the default options
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
