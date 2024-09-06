375387aaf72b32f0df8148aafbe64e388d05f45a
ViniciusBelasco <vbrafaeli@gmail.com>
2024-09-06 17:22:27 -0300

    Add html lsp

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,27 +1,30 @@
 return {
-	{
-		"nvimtools/none-ls.nvim",
-		config = function()
-			local null_ls = require("null-ls")
+   {
+      "nvimtools/none-ls.nvim",
+      config = function()
+         local null_ls = require("null-ls")
 
-			null_ls.setup({
-				sources = {
-					--Lua
-					null_ls.builtins.formatting.stylua,
-					--python
-					require('none-ls.diagnostics.flake8'),
-					null_ls.builtins.formatting.isort,
-					null_ls.builtins.formatting.black,
-          -- C
-          require('none-ls.diagnostics.cpplint'),
-          null_ls.builtins.formatting.clang_format
-				},
-			})
+         null_ls.setup({
+            sources = {
+               --Lua
+               null_ls.builtins.formatting.stylua,
+               --python
+               require("none-ls.diagnostics.flake8"),
+               null_ls.builtins.formatting.isort,
+               null_ls.builtins.formatting.black,
+               -- C
+               require("none-ls.diagnostics.cpplint"),
+               null_ls.builtins.formatting.clang_format,
+               -- Html, css, JS
+               null_ls.builtins.formatting.prettier,
+               null_ls.builtins.diagnostics.erb_lint
+            },
+         })
 
-			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-		end,
-		dependencies = {
-			"nvimtools/none-ls-extras.nvim",
-		},
-	},
+         vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+      end,
+      dependencies = {
+         "nvimtools/none-ls-extras.nvim",
+      },
+   },
 }
