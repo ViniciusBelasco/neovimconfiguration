ec3850a2991cd812169128b15a50e90a9deec4ff
ViniciusBelasco <vbrafaeli@gmail.com>
2024-10-15 13:18:05 -0300

    Remove init.lua from config directory

--- init.lua
+++ init.lua
@@ -1,1 +1,25 @@
-require("config")
+-- Example using a list of specs with the default options
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+   -- bootstrap lazy.nvim
+   -- stylua: ignore
+   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+      lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+require("lazy").setup("plugins", {
+   change_detection = {
+      enabled = true,
+      notify = false
+   }
+})
+
+require("config.globals")
+require("config.options")
+require("config.autocmds")
+require("config.keymaps")
+require("config.sgames")
