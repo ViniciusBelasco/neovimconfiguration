614ed71f4d2b9a405ae08cc931bef5c18962c3c3
ViniciusBelasco <vbrafaeli@gmail.com>
2024-10-16 13:00:01 -0300

    Change telescope configuration

--- lua/plugins/luasnip-config.lua
+++ lua/plugins/luasnip-config.lua
@@ -1,56 +1,56 @@
 return {
-	"L3MON4D3/LuaSnip",
-	version = "2.*",
-	build = "make install_jsregexp",
-	config = function()
-		local ls = require("luasnip")
+   "L3MON4D3/LuaSnip",
+   version = "2.*",
+   build = "make install_jsregexp",
+   config = function()
+      local ls = require("luasnip")
 
-		require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/snippets/" })
+      require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/snippets/" })
 
-		local types = require("luasnip.util.types")
+      local types = require("luasnip.util.types")
 
-		ls.config.set_config({
-			history = true,
-			updateevents = "TextChanged, TextChangedI",
-			enable_autosnippets = true,
-			ext_opts = {
-				[types.choiceNode] = {
-					active = {
-						virt_text = { { "●", "GruvboxOrange" } },
-					},
-				},
-			},
-		})
+      ls.config.set_config({
+         history = true,
+         updateevents = "TextChanged, TextChangedI",
+         enable_autosnippets = true,
+         ext_opts = {
+            [types.choiceNode] = {
+               active = {
+                  virt_text = { { "●", "GruvboxOrange" } },
+               },
+            },
+         },
+      })
 
-		--Key Maps
-		vim.keymap.set({ "i", "s" }, "<a-p>", function()
-			if ls.expand_or_jumpable() then
-				ls.expand()
-			end
-		end)
+      --Key Maps
+      vim.keymap.set({ "i", "s" }, "<a-p>", function()
+         if ls.expand_or_jumpable() then
+            ls.expand()
+         end
+      end)
 
-		vim.keymap.set({ "i", "s" }, "<a-k>", function()
-			if ls.jumpable(1) then
-				ls.jump(1)
-			end
-		end)
+      vim.keymap.set({ "i", "s" }, "<a-k>", function()
+         if ls.jumpable(1) then
+            ls.jump(1)
+         end
+      end)
 
-		vim.keymap.set({ "i", "s" }, "<a-j>", function()
-			if ls.jumpable(-1) then
-				ls.jump(-1)
-			end
-		end)
+      vim.keymap.set({ "i", "s" }, "<a-j>", function()
+         if ls.jumpable(-1) then
+            ls.jump(-1)
+         end
+      end)
 
-		vim.keymap.set({ "i", "s" }, "<a-l>", function()
-			if ls.choice_active() then
-				ls.change_choice(1)
-			end
-		end)
+      vim.keymap.set({ "i", "s" }, "<a-l>", function()
+         if ls.choice_active() then
+            ls.change_choice(1)
+         end
+      end)
 
-		vim.keymap.set({ "i", "s" }, "<a-h>", function()
-			if ls.choice_active() then
-				ls.choice_active(-1)
-			end
-		end)
-	end,
+      vim.keymap.set({ "i", "s" }, "<a-h>", function()
+         if ls.choice_active() then
+            ls.choice_active(-1)
+         end
+      end)
+   end,
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,62 +1,57 @@
-local config = function()
-   local telescope = require("telescope")
-   local builtinTc = require("telescope.builtin")
-   telescope.setup({
-      defaults = {
-         mappings = {
-            i = {
-               ["<C-j>"] = "move_selection_next",
-               ["<C-k>"] = "move_selection_previous",
-               -- Telescope
-               vim.keymap.set("n", "<leader>fk", function()
-                  builtinTc.keymaps()
-               end, {}),
-               vim.keymap.set("n", "<leader>fh", function()
-                  builtinTc.help_tags()
-               end, {}),
-               vim.keymap.set("n", "<leader>ff", function()
-                  builtinTc.find_files()
-               end, {}),
-               vim.keymap.set("n", "<leader>fa", function()
-                  builtinTc.builtin()
-               end, {}),
-               vim.keymap.set(
-                  "n",
-                  "<leader>fg",
-                  ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>"
-               ),
-               vim.keymap.set("n", "<leader>fb", function()
-                  builtinTc.buffers()
-               end, {}),
+return {
+   {
+      "nvim-telescope/telescope.nvim",
+      lazy = true,
+      dependencies = {
+         { "nvim-lua/plenary.nvim" },
+         {
+            "nvim-telescope/telescope-fzf-native.nvim",
+            build = "make",
+            cond = function()
+               return vim.fn.executable("make") == 1
+            end,
+         },
+         {
+            "nvim-telescope/telescope-live-grep-args.nvim",
+         },
+      },
+      opts = {
+         defaults = {
+            layout_config = {
+               vertical = {
+                  width = 0.75,
+               },
             },
          },
       },
-   })
-   telescope.load_extension("live_grep_args")
-end
+      config = function()
+         local telescope = require("telescope")
 
-local uiconfig = function()
-   require("telescope").setup({
-      extensions = {
-         ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-         },
-      },
-   })
-   require("telescope").load_extension("ui-select")
-end
+         -- first setup telescope
+         telescope.setup({
+            defaults = {
+               mappings = {
+                  i = {
+                     ["<C-j>"] = "move_selection_next",
+                     ["<C-k>"] = "move_selection_previous",
+                  },
+               },
+            },
+         })
 
-return {
-   {
-      "nvim-telescope/telescope-ui-select.nvim",
-      config = uiconfig,
+         -- then load the extension
+         telescope.load_extension("live_grep_args")
+      end,
    },
    {
-      "nvim-telescope/telescope.nvim",
-      dependencies = {
-         "nvim-lua/plenary.nvim",
-         "nvim-telescope/telescope-live-grep-args.nvim",
-      },
-      opts = config,
+      "nvim-telescope/telescope-ui-select.nvim",
+      config = function()
+         extensions = {
+            ["ui-select"] = {
+               require("telescope.themes").get_dropdown({}),
+            },
+            require("telescope").load_extension("ui-select"),
+         }
+      end,
    },
 }
